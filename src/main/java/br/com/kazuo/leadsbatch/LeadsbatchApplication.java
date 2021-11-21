ff2fee46d4b9ad02d6a151600adf722fc5d7c939
devilskw <kazuo_w@hotmail.com>
2021-11-21 10:26:06 -0300

    feat(initial): Initial project

---
+++ src/main/java/br/com/kazuo/leadsbatch/LeadsbatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.kazuo.leadsbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadsbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadsbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kazuo/leadsbatch/LeadsbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kazuo.leadsbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadsbatchApplicationTests {
+
+	//Test
+	void contextLoads() {
+	}
+
+}
